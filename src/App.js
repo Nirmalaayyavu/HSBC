6a3123a748e8edd363aa74731059a625db2ee0f1
Nirmala Ayyavu <nirmalaayyavu@gmail.com>
2021-04-08 16:19:16 +0530

    test cases for the two API fetch calls

--- src/App.js
+++ src/App.js
@@ -11,80 +11,87 @@ import * as Constant from './constant'
 
 function App() {
 
-  
-  const [timezoneList,setTimezoneList]= useState([])
-  const [display,setDefaultTime] = useState({})
-  const [title,setTitle] =useState (Constant.DROPDOWN_TITLE)
+
+  const [timezoneList, setTimezoneList] = useState([])
+  const [display, setDefaultTime] = useState({})
+  const [title, setTitle] = useState(Constant.DROPDOWN_TITLE)
 
 
-  const fetchData=()=>{
+  const fetchData = () => {
     const timezoneAPI = 'http://api.timezonedb.com/v2.1/list-time-zone?key=XWSLLPX5RMIZ&format=json&zone=Europe/*'
     const defaultTimezoneAPI = 'http://api.timezonedb.com/v2/get-time-zone?key=XWSLLPX5RMIZ&format=json&by=zone&zone=Europe/London'
-    
+
     const getTimeZoneResponse = axios.get(timezoneAPI)
     const getDefaultTimeResponse = axios.get(defaultTimezoneAPI)
 
-    axios.all([getTimeZoneResponse,getDefaultTimeResponse]).then(
-      axios.spread((...response)=>{
+     axios.all([getTimeZoneResponse, getDefaultTimeResponse]).then(
+      axios.spread((...response) => {
         const timeZoneList = response[0].data.zones
         const defaultData = {
-          name:response[1].data.zoneName,
-          time:response[1].data.formatted
+          name: response[1].data.zoneName,
+          time: response[1].data.formatted
         }
         setTimezoneList(timeZoneList)
         setDefaultTime(defaultData)
-
+        
       })
-    ).catch((errors)=>{
+    ).catch((errors) => {
       console.log("error occured ")
     })
   }
-  
 
-  const getUpdateTime =(zoneName)=>{
+
+  const getUpdateTime = (zoneName) => {
     axios.get(`http://api.timezonedb.com/v2/get-time-zone?key=XWSLLPX5RMIZ&format=json&by=zone&zone=${zoneName}`)
-    .then(response=>{
-      setDefaultTime({
-        name:response.data.zoneName,
-        time:response.data.formatted
+      .then(response => {
+        setDefaultTime({
+          name: response.data.zoneName,
+          time: response.data.formatted
+        })
       })
-    })
-    .catch((error)=>{
-      console.log("error occured")
-    })
+      .catch((error) => {
+        console.log("error occured")
+      })
   }
-  const handleChange =(value)=>{
+  const handleChange = (value) => {
     setTitle(value)
     getUpdateTime(value)
   }
- 
-  useEffect(()=>{
-    fetchData()
-    
-  },[])
 
-  useEffect(()=>{
+  useEffect(() => {
+    fetchData()
+
+  }, [])
+
+  useEffect(() => {
     setInterval(() => {
-      if(title !==Constant.DROPDOWN_TITLE)
-      handleChange(title)
+      if (title !== Constant.DROPDOWN_TITLE)
+        handleChange(title)
     }, 5000);
-  },[title])
+  }, [title])
 
   return (
     <div className="App">
-      <h1>React Timezone Example</h1>
-      <br/>
-      <h2> Time in  {display.name? display.name.replace('/',' - '):''} : {moment(display.time).format("hh:mm A")}</h2>
-      <br/>
-      <DropdownButton id="country-list" title={title}>
-        {timezoneList.length>0 && timezoneList.map((items,i)=>(
-        <Dropdown.Item value={items.zoneName} key={i} 
-        onSelect={()=>handleChange(items.zoneName)}>
-          {items.zoneName.replace('/',' - ')}</Dropdown.Item>
-        ))}
-      </DropdownButton>
+      {timezoneList.length === 0 ? (
+        <div data-testid="loader">Loading...</div>
+      ) : (
+        <div data-testid="timerSection">
+          <h1>React Timezone Example</h1>
+          <br />
+          <h2 data-testid="time-display"> Time in  {display.name ? display.name.replace('/', ' - ') : ''} : {moment(display.time).format("hh:mm A")} </h2>
+          <br />
+          <DropdownButton id="country-list"   title={title}>
+            {timezoneList.map((items, i) => (
+              <Dropdown.Item value={items.zoneName} key={i}
+                onSelect={() => handleChange(items.zoneName)} data-testid="list">
+                {items.zoneName.replace('/', ' - ')}</Dropdown.Item>
+            ))}
+          </DropdownButton>
+          </div>
+      )}
     </div>
-  );
+  )
+      
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from 'react-dom';
+import { render,  waitFor, act, cleanup,fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import axiosMock from "axios";
+import App from "./App";
+import * as Constant from './constant'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+
+afterEach(cleanup)
+
+
+test("check Initial render ", async () => {
+  const { getByTestId } = render(<App />)
+  expect(getByTestId('loader')).toHaveTextContent('Loading..')
+})
+
+test("check fetch data call on useEffect part", async () => {
+  axiosMock.get = jest
+    .fn()
+    .mockResolvedValueOnce({
+      data: { 
+        "zones": [
+          { "countryCode": "AD", "countryName": "Andorra", "zoneName": "Europe\/Andorra", "gmtOffset": 7200, "timestamp": 1617866241 },
+          { "countryCode": "AE", "countryName": "United Arab Emirates", "zoneName": "Asia\/Dubai", "gmtOffset": 14400, "timestamp": 1617873441 }
+        ] }
+    })
+    .mockResolvedValueOnce({
+      data: {
+        "zoneName": "Europe\/Andorra",
+        "formatted": 1617866241
+      }
+    })
+
+  // Spy on spread method so that we can wait for it to be called.
+  jest.spyOn(axiosMock, "spread");
+  const { getByTestId } = render(<App />);
+  await waitFor(() =>{ 
+    expect(axiosMock.get).toHaveBeenCalledTimes(2)
+    expect(axiosMock.spread).toHaveBeenCalledTimes(1)
+    expect(getByTestId('time-display')).toHaveTextContent("Time in Europe - Andorra : 10:54 PM")
+    expect(screen.getByText(Constant.DROPDOWN_TITLE)).toBeDefined() 
+    
+
+  });
+  
+
+})
+
+
+
+
